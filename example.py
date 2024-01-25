6a8b44d1e935463c5f16375c72818ecb4916d4a1
Luke Atazona <144291045+Atazonomics@users.noreply.github.com>
2024-01-25 16:24:58 +0100

    modified name from Wu to Jon

---
+++ example.py
@@ -0,0 +1,11 @@
+import lukewu_library.math.calculation as m
+import pytest
+
+print(m.addtion(1,2))
+
+def test_some_value_with_addition():
+    assert m.addtion(1,2) == 3
+    
+    
+def test_some_value_with_addition():
+    assert m.addtion(1,2) == 4
